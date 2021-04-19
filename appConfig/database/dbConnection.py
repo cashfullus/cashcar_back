dd7a3b75ac75c1970818bfce19f7fb80cfc83037
pjkfckr <pjkfckr@gmail.com>
2021-04-19 11:26:11 +0900

    광고 신청 기능 추가

--- appConfig/database/dbConnection.py
+++ appConfig/database/dbConnection.py
@@ -73,6 +73,7 @@ class Database:
         rows = self.cursor.fetchall()
         return rows
 
+    # 광고 디테일 By ad_id
     def getOneAdByAdId(self, ad_id):
         sql = "SELECT " \
               "ad_id, title, title_image, logo_image, " \
@@ -84,3 +85,76 @@ class Database:
         self.cursor.execute(query=sql, args=ad_id)
         row = self.cursor.fetchone()
         return row
+
+    # 광고 신청할때 사용 by ad_id
+    def getOneAdApplyByAdId(self, ad_id):
+        sql = "SELECT " \
+              "ad_id, title, title_image, logo_image, " \
+              "DATE_FORMAT(recruit_start_date, '%%Y-%%m-%%d %%H:%%i:%%s') as recruit_start_date, " \
+              "DATE_FORMAT(recruit_end_date, '%%Y-%%m-%%d %%H:%%i:%%s') as recruit_end_date, " \
+              "activity_period, max_recruiting_count, recruiting_count, " \
+              "total_point, area, description " \
+              "FROM ad_information WHERE " \
+              "recruiting_count < max_recruiting_count " \
+              "AND NOW() BETWEEN recruit_start_date AND recruit_end_date AND " \
+              "ad_id = %s"
+        self.cursor.execute(query=sql, args=ad_id)
+        row = self.cursor.fetchone()
+        return row
+
+    # 광고 신청한 건에 대하여 By ad_user_apply_id
+    def getOneAdUserApplyById(self, ad_user_apply_id):
+        sql = "SELECT " \
+              "ad_user_apply_id, user_id, ad_id, status, " \
+              "DATE_FORMAT(register_time, '%%Y-%%m-%%d %%H:%%i:%%s') as register_time, " \
+              "DATE_FORMAT(change_status_time, '%%Y-%%m-%%d %%H:%%i:%%s') as change_status_time " \
+              "FROM ad_user_apply WHERE ad_user_apply_id = %s"
+        self.cursor.execute(query=sql, args=ad_user_apply_id)
+        row = self.cursor.fetchone()
+        return row
+
+    # 광고 신청한 건에 대하여
+    def getAllAdUserApply(self):
+        sql = "SELECT " \
+              "ad_user_apply_id, user_id, ad_id, status, " \
+              "DATE_FORMAT(register_time, '%Y-%m-%d %H:%i:%s') as register_time, " \
+              "DATE_FORMAT(change_status_time, '%Y-%m-%d %H:%i:%s') as change_status_time " \
+              "FROM ad_user_apply"
+        self.cursor.execute(query=sql)
+        rows = self.cursor.fetchall()
+        return rows
+
+    # 신청한 광고의 status 만 가져오기
+    def getOneApplyStatus(self, ad_user_apply_id):
+        sql = "SELECT status FROM ad_user_apply WHERE ad_user_apply_id = %s"
+        self.cursor.execute(query=sql, args=ad_user_apply_id)
+        row = self.cursor.fetchone()
+        return row
+
+    # 광고 신청 수락 후 id 데이터 가져오기
+    def getAdMissionCardIdsByAcceptApply(self, ad_user_apply_id):
+        sql = "SELECT ad_mission_card_id, mc.mission_card_id, due_date " \
+              "FROM ad_user_apply " \
+              "JOIN ad_mission_card amc on ad_user_apply.ad_id = amc.ad_id " \
+              "JOIN mission_card mc on amc.mission_card_id = mc.mission_card_id " \
+              "WHERE ad_user_apply_id = %s"
+        self.cursor.execute(query=sql, args=ad_user_apply_id)
+        rows = self.cursor.fetchall()
+        return rows
+
+    def getMainMyAd(self, user_id):
+        sql = "SELECT " \
+              "title, logo_image, ad_mission_card_user_id, additional_mission_success_count, " \
+              "default_mission_success_count, amcu.status, " \
+              "DATE_FORMAT(activity_start_date, '%%Y-%%m-%%d %%H:%%i:%%s') as activity_start_date, " \
+              "DATE_FORMAT(activity_end_date, '%%Y-%%m-%%d %%H:%%i:%%s') as activity_end_date " \
+              "FROM ad_mission_card_user as amcu " \
+              "JOIN ad_user_apply aua on amcu.ad_user_apply_id = aua.ad_user_apply_id " \
+              "JOIN ad_information ai on aua.ad_id = ai.ad_id " \
+              "WHERE aua.status != 'done' AND amcu.status != 'stand_by' AND user_id = %s"
+        self.cursor.execute(query=sql, args=user_id)
+        row = self.cursor.fetchone()
+        return row
+
+
+
--- appConfig/models/ad_model.py
+++ appConfig/models/ad_model.py
@@ -1,12 +1,15 @@
 from ..database.dbConnection import Database
 from werkzeug.utils import secure_filename
+
+from datetime import date, timedelta, datetime
+
 import os
 
 BASE_IMAGE_LOCATION = os.getcwd() + "/CashCar/appConfig/static/image/adverting"
 BASE_IMAGE_LOCATION_BACK = "/appConfig/static/image/adverting"
 
 
-# 광고 등록 / (현재 이미지는 미정 2021-04-13 )
+# 광고 등록 / (현재 이미지는 미정 2021-04-13 ) title_image 의 첫번째는 무조건 썸네일 이미지가 된다.
 def register(image_dict, **kwargs):
     db = Database()
     sql = "INSERT INTO ad_information " \
@@ -50,7 +53,7 @@ def register(image_dict, **kwargs):
 # 광고 리스트 (parameter query_string)
 def get_all_by_category_ad_list(page, category):
     db = Database()
-    per_page = (page-1)*20
+    per_page = (page - 1) * 20
     start_at = per_page + 20
     status = {"correct_category": True}
     sql_parameter_val = ""
@@ -113,20 +116,146 @@ def get_information_for_ad_apply(user_id, ad_id):
 
     return result, status
 
+
+# 광고 신청 POST
+def ad_apply(user_id, ad_id, **kwargs):
+    db = Database()
+    status = {"user_information": True, "ad_information": True, "already_apply": True}
+    target_user = db.getUserById(user_id)
+    target_ad = db.getOneAdApplyByAdId(ad_id)
+    already_apply_ad = db.executeOne(
+        query="SELECT ad_user_apply_id FROM ad_user_apply WHERE status in (0, 1) and user_id = %s",
+        args=user_id
+    )
+
+    if not target_ad:
+        status["ad"] = False
+        return status
+
+    elif not target_user:
+        status["user"] = False
+        return status
+
+    elif already_apply_ad:
+        status["already_apply"] = False
+        return status
+
+    else:
+        db.execute(
+            query="UPDATE user SET main_address = %s, "
+                  "detail_address = %s, call_number = %s, name = %s "
+                  "WHERE user_id = %s",
+            args=[kwargs['main_address'], kwargs['detail_address'], kwargs['call_number'], kwargs['name'], user_id]
+        )
+        db.execute(
+            query="INSERT INTO ad_user_apply (user_id, ad_id, register_time) VALUES (%s, %s, NOW())",
+            args=[user_id, ad_id]
+        )
+        db.execute(
+            query="UPDATE ad_information SET recruiting_count = recruiting_count + 1 WHERE ad_id = %s",
+            args=ad_id
+        )
+        db.commit()
+
+        return status
+
+
+# 진행중인 광고 By User
+def get_ongoing_ad(user_id):
+    db = Database()
+    result = db.executeOne(
+        query="SELECT "
+              "* "
+              "FROM ad_mission_card_user as amcu "
+              "JOIN ad_user_apply aua on amcu.ad_user_apply_id = aua.ad_user_apply_id AND aua.user_id = %s",
+        args=user_id
+    )
+    return result
+
+
+# 광고 신청 목록
+def ad_apply_list():
+    db = Database()
+    result = db.getAllAdUserApply()
+    return result
+
+
+# 광고신청 목록 -> Detail
+def get_ad_apply(ad_user_apply_id):
+    db = Database()
+    result = db.getOneAdUserApplyById(ad_user_apply_id=ad_user_apply_id)
+    if result:
+        return result
+    else:
+        return False
+
+
+# 메인화면 본인이 진행중인 광고 카드의 정보
+def get_ongoing_user_by_id(user_id):
+    db = Database()
+    result = db.getMainMyAd(user_id=user_id)
+    if result:
+        return result
+    else:
+        return False
+
+
+# 신청한 광고 status 업데이트
+# 먼저 광고와 연결된 미션 을 전부 return
+# 그 해당 미션에 대해 미션하기를 누를시 데이터 생성
 #
-# # 광고 신청 POST
-# def ad_apply(user_id, ad_id, **kwargs):
-#     db = Database()
-#     target_user = db.getUserById(user_id)
-#     target_ad = db.getOneAdByAdId(ad_id)
-#
-#     if target_ad and target_user:
-#
+# 승인시 flow (default = stand_by(승인 대기중) -> accept(승인) -> during_delivery(배송중) -> delivery_completed(배송완료)
+# 거절시 flow (default = stand_by(승인 대기중) -> reject(거절)
+def update_ad_apply_status(ad_user_apply_id, **kwargs):
+    db = Database()
+    apply_information = {"rejected": True, "accept": True}
+    apply_status = db.getOneApplyStatus(ad_user_apply_id=ad_user_apply_id)
+    if apply_status["status"] == "reject" and kwargs["status"] == "reject":
+        apply_information["rejected"] = False
+        return apply_information
 
+    elif apply_status["status"] == "accept" and kwargs["status"] == "accept":
+        apply_information["accept"] = False
+        return apply_information
 
+    else:
+        if kwargs["status"] == "reject":
+            # ad_user_apply 테이블에서 ad_id 가 같은 ad_information 테이블에서 모집인원 -1 (ad_user_apply_id)에 맞는 데이터
+            db.execute(
+                query="UPDATE ad_information as ad_info "
+                      "JOIN ad_user_apply aua on ad_info.ad_id = aua.ad_id "
+                      "SET ad_info.recruiting_count = ad_info.recruiting_count - 1 "
+                      "WHERE aua.ad_user_apply_id = %s",
+                args=ad_user_apply_id
+            )
 
-
-
-
-
-
+        elif kwargs["status"] == "accept":
+            ad_mission_card_info = db.getAdMissionCardIdsByAcceptApply(ad_user_apply_id=ad_user_apply_id)
+            # 수락후 미션 생성
+            for i in range(len(ad_mission_card_info)):
+                if ad_mission_card_info[i]["mission_card_id"] == 1:
+                    first_start_date = datetime.now().strftime('%Y-%m-%d %H:%M:%S')
+                    first_end_date = date.today() + timedelta(days=ad_mission_card_info[i]["due_date"])
+                    db.execute(
+                        query="INSERT INTO "
+                              "ad_mission_card_user "
+                              "(ad_user_apply_id, ad_mission_card_id, status, "
+                              "mission_start_date, mission_end_date, register_time) "
+                              "VALUES (%s, %s, %s, %s, %s, NOW())",
+                        args=[ad_user_apply_id, ad_mission_card_info[i]["ad_mission_card_id"], "ongoing",
+                              first_start_date, first_end_date.strftime('%Y-%m-%d 23:59:59')]
+                    )
+                else:
+                    db.execute(
+                        query="INSERT INTO "
+                              "ad_mission_card_user "
+                              "(ad_user_apply_id, ad_mission_card_id, register_time) "
+                              "VALUES (%s, %s, NOW())",
+                        args=[ad_user_apply_id, ad_mission_card_info[i]["ad_mission_card_id"]]
+                    )
+        db.execute(
+            query="UPDATE ad_user_apply SET status = %s WHERE ad_user_apply_id = %s",
+            args=[kwargs['status'], ad_user_apply_id]
+        )
+        db.commit()
+        return apply_information
