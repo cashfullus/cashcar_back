4868e4bee07d61b1ae919c2456870c2b38baa7d3
pjkfckr <pjkfckr@gmail.com>
2021-04-22 15:32:43 +0900

    광고 등록 기능 수정

--- appConfig/models/mission_model.py
+++ appConfig/models/mission_model.py
@@ -5,45 +5,7 @@ import os
 BASE_IMAGE_LOCATION = os.getcwd() + "/CashCar/appConfig/static/image/mission"
 
 
-# 미션카드 등록
-# def register(**kwargs):
-#     db = Database()
-#     sql = "INSERT INTO mission_card " \
-#           "(mission_type, mission_name, additional_point, due_date) " \
-#           "VALUES " \
-#           "(%s, %s, %s, %s)"
-#     value_list = [
-#         kwargs.get('mission_type'),
-#         kwargs.get('mission_name'),
-#         kwargs.get('additional_point'),
-#         kwargs.get('due_date')
-#     ]
-#     db.execute(query=sql, args=value_list)
-#     db.commit()
-#     return True
-
-
-# 미션 이미지 등록
-def mission_save_images(image_dict):
-    db = Database()
-    save_to_db_dict = {}
-    directory = f"{BASE_IMAGE_LOCATION}"
-    os.makedirs(directory, exist_ok=True)
-    for key, val in image_dict.items():
-        # save_db = BASE_IMAGE_LOCATION_BACK + "/" + secure_filename(val.filename)
-        val.save(directory + "/" + secure_filename(val.filename))
-        save_to_db_dict.setdefault(key, directory + "/" + secure_filename(val.filename))
-
-    db.execute(
-        query="INSERT INTO mission_images (side_image, back_image) VALUES (%s, %s)",
-        args=[save_to_db_dict["side_image"], save_to_db_dict["back_image"]]
-    )
-    db.commit()
-
-    return True
-
-
-# 미션 입
+# 미션 데이터 정보
 def get_mission_type_idx_by_stand_by(ad_mission_card_user_id):
     db = Database()
     amcu_id_and_mission_type = db.executeOne(
