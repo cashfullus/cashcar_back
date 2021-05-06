74ed351817ed46c5e128176ffabc2b1fe05b13dd
pjkfckr <pjkfckr@gmail.com>
2021-05-06 10:25:42 +0900

    Server Merged

--- wsgi.py
+++ wsgi.py
@@ -1,4 +1,4 @@
 from appConfig.routes import app
 
 if __name__ == '__main__':
-    app.run(ssl_context=('/etc/letsencrypt/live/app.api.service.cashcarplus.com/fullchain.pem', '/etc/letsencrypt/live/app.api.service.cashcarplus.com/privkey.pem'))
+    app.run()
