cffe803680d5c4c921b169615f13b7834f1be0f6
pjkfckr <pjkfckr@gmail.com>
2021-05-28 18:16:39 +0900

    my-ad 수정

--- appConfig/models/ad_model.py
+++ appConfig/models/ad_model.py
@@ -492,8 +492,10 @@ class UserMyAd:
             check_days = (end_date - start_date).days
             if (datetime.now().date() - start_date).days > 0:
                 ad_information['point'] = (datetime.now().date() - start_date).days * ad_information['point']
-            # time_diff = ((date.today() + timedelta(days=1)) - start_date).days
-            time_diff = (ad_information['activity_period'] - check_days)
+            time_diff = ((date.today() + timedelta(days=1)) - start_date).days
+            if time_diff > check_days:
+                time_diff = check_days
+            # time_diff = (ad_information['activity_period'] - check_days)
             day_diff = ((time_diff / ad_information['activity_period']) * 100)
             ad_information['ongoing_day_percent'] = int(day_diff)
             ad_information['ongoing_days'] = time_diff
